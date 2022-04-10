11bb47d9c574578e9dd5fc41dd8c9b0795f34d8e
sapatmohit <103207988+sapatmohit@users.noreply.github.com>
2022-04-10 21:19:49 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", function () {
+    /*---------------page loader-------------------*/
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(function () {
+        document.querySelector(".page-loader").style.display = "none";
+    }, 600)
+    /*------------- animation on scroll -------------------- */
+    AOS.init();
+})
+
+/* ---------------- toggle navbar -------------------*/
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+
+/* -------close nav after clicking on a nav item--------- */
+
+document.addEventListener("click", function (e) {
+    if (e.target.closest(".nav-item")) {
+        toggleNav();
+    }
+});
+
+/*--------- sticky header ---------*/
+
+window.addEventListener("scroll", function () {
+    if (this.pageYOffset > 60) {
+        document.querySelector(".header").classList.add("sticky");
+    } else {
+        document.querySelector(".header").classList.remove("sticky")
+    }
+});
+
+/*-------------- menu tabs ----------*/
+
